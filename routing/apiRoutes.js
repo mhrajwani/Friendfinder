a237d0d4faed8bde60571fe375da04a9478ceade
mhrajwani <mo.rajwani@gmail.com>
2018-05-05 00:24:15 -0400

    done

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,34 +9,36 @@ module.exports = function(app) {
 
 
   app.post("/api/friend", function(req, res) {
-    var scores= [];
-    var totaldifference=[];
-   //var newFriend = req.body;
-   for(i=0;i<friends.length;i++){
-     scores.push(friends[i].scores);
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
 
-   }
-  
-   for(i=0;i<10;i++){
-    var diff = scores[0][i] - req.body.scores[i]
-    diff = Math.abs(diff)
-    totaldifference.push(diff);
-   }
-   for(i=0;i<friends.length;i++){
-    
+    //runs through all current friends in list
+    for(var i=0; i<friends.length; i++){
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newFriendScores[j])));
+      }
 
-  }
+      //push results into scoresArray
+      scoresArray.push(scoresDiff);
+    }
 
-   
-   var bestMatch = Math.min.apply(null, totaldifference) 
-   console.log("this is the best:" + bestMatch)
+    //after all friends are compared, find best match
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
 
-    console.log("this is totaldiff: "+totaldifference);
-    console.log(req.body.scores[1]);
-  
+    //return bestMatch data
+    var bff = friends[bestMatch];
+    res.json(bff);
+
+    //pushes new submission into the friendsList array
     friends.push(req.body);
-  
-    res.json(req.body);
 
    
   });
